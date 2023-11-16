9aaa8f50d83d107c47aabe994df758fe60876644
vinil kanojiya <41778036+vinilkanojiya@users.noreply.github.com>
2023-11-16 20:44:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,5 +27,37 @@ else
 console.log(randomNumber1, randomNumber2, win);
 
 document.getElementsByTagName("h1")[0].textContent = win; 
-// same thing we can do with innerhtml but the text content only focus on text of inside the tag and innerhtml focus on whole code.
+// same thing we can do with inner HTML but the text content only focuses on text inside the tag and inner HTML focuses on the whole code.
 
+//WITHOUT ARRAY
+/*
+//code for player 1 tile
+var randomNumber1  = Math.round(Math.random()*5);
+var result1 = "./images/dice"+ randomNumber1+".png" ;
+document.querySelector(".img1").setAttribute("src", result1);
+
+//code for player 2 tile
+var randomNumber2  = Math.round(Math.random()*5);
+var result2 = "./images/dice" + randomNumber2+".png" ;
+document.querySelector(".img2").setAttribute("src", result2);
+var win;
+
+if(randomNumber1<randomNumber2)
+{
+    win = "Winner is Player 2";
+}
+else if (randomNumber1>randomNumber2) {
+
+    win = "Winner is player 1";
+}
+else
+{
+    win = "It's TIE."
+}
+
+console.log(randomNumber1, randomNumber2, win);
+
+document.getElementsByTagName("h1")[0].textContent = win; 
+
+
+*/
